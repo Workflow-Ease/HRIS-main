38ea299517b2470116f1e64b8620aecd9a398868
hyunsungJoo <joo@LAPTOP-M7ISC7UR.>
2024-02-07 09:06:21 +0900

    update

--- src/views/components/Joo.vue
+++ src/views/components/Joo.vue
@@ -1,30 +1,39 @@
 <template>
-      <div class="col-lg-7 mb-lg-0 mb-4">
-        <div class="card">
-          <div class="p-3 pb-0 card-header">
-            <div class="d-flex justify-content-between">
-              <h6 class="mb-2">부서별 급여</h6>
-            </div>
-          </div>
-          <div class="table-responsive">
-            <table class="table align-items-center">
-              <thead>
-                <tr>
-                  <th>부서</th>
-                  <th>급여</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(department, index) in departments" :key="index">
-                  <td>{{ department.name }}</td>
-                  <td>{{ department.salary }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+  <div class="card">
+    <div class="p-3 pb-0 card-header">
+      <div class="d-flex justify-content-between">
+        <h6 class="mb-2">부서별 성과</h6>
       </div>
-      <div class="col-lg-5">
-      </div>
-  </template>
+    </div>
+    <div class="table-responsive">
+      <table class="table text-center">
+        <thead>
+          <tr>
+            <th>부서 이름</th>
+            <th>부서 성과</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(department, index) in uniqueDepartments" :key="index">
+            <td class="text-center">{{ department.departmentName }}</td>
+            <td class="text-center">{{ department.departmentPerformance }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
 
+<script>
+export default {
+  computed: {
+    uniqueDepartments() {
+      const unique = {};
+      this.top5Departments.forEach(department => {
+        unique[department.departmentName] = department;
+      });
+      return Object.values(unique);
+    }
+  }
+}
+</script>
